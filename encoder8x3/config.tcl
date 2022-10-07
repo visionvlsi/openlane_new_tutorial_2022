ef2c8debc4af75f55ee0526932bc370ec2c0e724
Vision-VLSI Industry PVT. LTD <vvi.pvtltd@gmail.com>
2022-10-07 11:55:14 +0530

    Add files via upload

---
+++ encoder8x3/config.tcl
@@ -0,0 +1,14 @@
+
+set ::env(DESIGN_NAME) {encoder8x3}
+set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
+set ::env(CLOCK_TREE_SYNTH) 0
+set ::env(CLOCK_PORT) ""
+set ::env(CLOCK_PERIOD) "10.0"
+set ::env(FP_CORE_UTIL) 3
+set ::env(PL_TARGET_DENSITY) 0.5
+set ::env(DESIGN_IS_CORE) {1}
+
+set tech_specific_config "$::env(DESIGN_DIR)/$::env(PDK)_$::env(STD_CELL_LIBRARY)_config.tcl"
+if { [file exists $tech_specific_config] == 1 } {
+    source $tech_specific_config
+}
